d3d1681204fb6f4e9abcdbf849f8e09a265d6022
xzwsituonline <xiongzw@situonline.com>
2014-09-17 10:40:08 +0800

    version.php

---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+		'android' => array(
+			 'version' => '1.0',             //版本号
+			 'link' => "http://".$_SERVER['HTTP_HOST']."/wzt.apk",    //下载地址
+			 'update' => array(),           //更新内容
+		),
+		'ios' => array(
+				'version' => '1.0',             //版本号
+				'link' => "http://".$_SERVER['HTTP_HOST']."/wzt.ipa",  //下载地址
+				'update' => array(),           //更新内容
+		),
+);
